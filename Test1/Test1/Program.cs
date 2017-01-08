45c714827ad00a038d540dd26c6a832480ae4261
ADT <Adrian@VMDEVADT>
2017-01-08 14:38:06 +0000

    Master template

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Shape
+    {
+        int sides;
+        string type;
+        string description;
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
